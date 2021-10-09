a85378ef66598c697256c804901102a403d42d91
Ryan-Nam <alfofh7060@gmail.com>
2021-10-09 14:58:12 +1000

    Add axios PostMan

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Contents = () => {
+
+  // 마운트되었을때 바로 실행하는 효과를 주기 위해
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
+      console.log(res)
+    }
+    fetchEvents()
+  })
   return (
     <section>
         <h2>국내 코로나 현황</h2>
