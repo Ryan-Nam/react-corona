9c692686348ebb0ff6091fcf557b33791913ac9a
Ryan-Nam <alfofh7060@gmail.com>
2021-10-09 14:44:59 +1000

    Add CSS

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header className="header">
+        <h1>COVID-19</h1>
+        <select>
+          <option>Korea</option>
+          <option>Australia</option>
+        </select>
+      </header>
+  );
+}
+
+export default header
